ad6fc43dd3b74474222a1fc3d515a3508b74ff98
NickBanken <nick.banken@gmail.com>
2022-11-23 12:21:04 +0100

    addition of state todo

--- src/components/ListTodos.jsx
+++ src/components/ListTodos.jsx
@@ -1,20 +1,47 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 const ListTodos = () => {
+  const initialList = [
+    { name: "Learn react", done: false, id: uuidv4() },
+    { name: "Learn tailwind", done: false, id: uuidv4() },
+    { name: "Finish project", done: false, id: uuidv4() },
+  ];
+
+  const [todos, setTodos] = useState(initialList);
+
+  const handleCheck = (id) => {
+    const newList = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    });
+    setTodos(newList);
+  };
+
   return (
     <ul className="p-5">
-      <li>
-        <input className="mr-3" type="checkbox" id="todo1" />
-        <label htmlFor="todo1">Learn react.</label>
-      </li>
-
-      <li>
-        <input className="mr-3" type="checkbox" id="todo2" />
-        <label htmlFor="todo2">Learn tailwind.</label>
-      </li>
-
-      <li>
-        <input className="mr-3" type="checkbox" id="todo3" />
-        <label htmlFor="todo3">Finish project.</label>
-      </li>
+      {todos.map((todo) => {
+        return (
+          <li key={todo.id}>
+            <input
+              className="mr-3"
+              type="checkbox"
+              id={todo.id}
+              onChange={() => {
+                handleCheck(todo.id);
+              }}
+            />
+            <label
+              className={todo.done ? "line-through" : "no-underline"}
+              htmlFor={todo.id}
+            >
+              {todo.name}.
+            </label>
+          </li>
+        );
+      })}
     </ul>
   );
 };
